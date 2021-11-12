6c0c2cc70e9587618cf4892fcd183f057d69fd3d
evanvvav <ivankolesov.fi@gmail.com>
2021-11-12 19:14:00 +0200

    12.11 BootsTrap CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import QuestionPage from "./components/QuestionPage"
+import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap'
 
 
 function App() {
@@ -10,8 +11,27 @@ function App() {
 
   
   return (
-     <div className="container">
+     <div >
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">React-Survey</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#">Function1</Nav.Link>
+      <Nav.Link eventKey={2} href="#">Function2</Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#">Function3</Nav.Link>
+      <Nav.Link eventKey={2} href="#">Function4</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+      <div className="container">
+        
       <QuestionPage/>
+      </div>
       
 
         
